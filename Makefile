00d89b3655daba6919d0d456f59f55dd6f407bfa
Ryan Holanda <ryanholanda08@gmail.com>
2024-03-17 18:19:23 -0300

    chore: added make commands for testing anc code coverage

--- Makefile
+++ Makefile
@@ -1,2 +1,12 @@
+open-coverage:
+	open coverage/html/index.html
+
+gen-coverage:
+	forge coverage --report lcov --report-file coverage/lcov.info --silent \
+	&& genhtml coverage/lcov.info -o coverage/html/ --quiet
+
 setup:
 	@git config core.hooksPath lib/githooks
+
+tests:
+	@bash script/shell-script/test.sh
---
+++ script/shell-script/test.sh
@@ -0,0 +1,11 @@
+forge test -vvv \
+	&& rm -rf coverage \
+	&& mkdir coverage \
+	&& make gen-coverage
+
+
+if ! lcov --summary coverage/lcov.info --fail-under-lines `cat min.coverage` --quiet; then
+	echo -e "\033[0;31mCurrent tests coverage is less than 100%. Please consider increasing it\033[0m"
+	make open-coverage
+	exit 1
+fi
